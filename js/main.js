0d9cb78062b7e811b65c35fbf13b438eec261530
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-05-31 14:11:58 +0200

    Venues via Foursquare API

--- js/main.js
+++ js/main.js
@@ -14,18 +14,18 @@ function getLocation() {
 function showPosition(position) {
     coordLat = position.coords.longitude;
     coordLong = position.coords.latitude;
-    requestWeatherData(coordLat,coordLong);
+    requestWeatherData(coordLat, coordLong);
 
 }
 getLocation();
 var request = new XMLHttpRequest();
-function requestWeatherData(lat,lon){
-    var APIKey= "9e55039796c9a5f733b4100a7273622d";
+function requestWeatherData(lat, lon) {
+    var APIKey = "9e55039796c9a5f733b4100a7273622d";
     // api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
     // https://api.openweathermap.org/data/2.5/weather?lat=51.9541&lon=7.6210&appid=9e55039796c9a5f733b4100a7273622d
 
-    var apicall="https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+APIKey;
-    var weather=weatherData(apicall);
+    var apicall = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+    var weather = weatherData(apicall);
     console.log(weather);
     console.log(weather.weather);
     console.log(weather.weather[0].description);
@@ -37,9 +37,9 @@ function requestWeatherData(lat,lon){
     console.log(weatherText);
 }
 
-function weatherData(apicall){
+function weatherData(apicall) {
     request.open('GET', apicall, false);
-    request.onload = function() {
+    request.onload = function () {
         var data = JSON.parse(this.response);
     }
     //{"coord":{"lon":7.62,"lat":51.95},"weather":[{"id":521,"main":"Rain","description":"shower rain","icon":"09d"}],"base":"stations","main":{"temp":285.15,"feels_like":280.09,"temp_min":283.15,"temp_max":287.04,"pressure":1000,"humidity":71},"visibility":10000,"wind":{"speed":6.2,"deg":300},"clouds":{"all":40},"dt":1588347256,"sys":{"type":1,"id":1269,"country":"DE","sunrise":1588305552,"sunset":1588359227},"timezone":7200,"id":2867543,"name":"Münster","cod":200}
@@ -56,22 +56,22 @@ wholink =
     'i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
 L.tileLayer(
     'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: '&copy; '+mapLink+', '+wholink,
-        maxZoom: 20,
-    }).addTo(map);
+    attribution: '&copy; ' + mapLink + ', ' + wholink,
+    maxZoom: 20,
+}).addTo(map);
 console.log(map);
 var current_position;
-var lat,lon;
+var lat, lon;
 function onLocationFound(e) {
     if (current_position) {
         map.removeLayer(current_position);
     }
-    current_position= L.marker(e.latlng).addTo(map);
+    current_position = L.marker(e.latlng).addTo(map);
     console.log(map)
 
-    var latLngs = [ current_position.getLatLng() ];
-    lat=latLngs[0].lat;
-    lon=latLngs[0].lng;
+    var latLngs = [current_position.getLatLng()];
+    lat = latLngs[0].lat;
+    lon = latLngs[0].lng;
     map.panTo(new L.LatLng(lat, lon));
 
 }
@@ -83,17 +83,17 @@ map.on('locationerror', onLocationError);
 
 // wrap map.locate in a function
 function locate() {
-    map.locate({setView: true, maxZoom: 20});
+    map.locate({ setView: true, maxZoom: 20 });
 }
 locate();
 // call locate every 3 seconds... forever
 setInterval(locate, 5000);
 //locate()
-L.easyButton('<img src="./crosshairs-gps.png">', function(btn, map){
-    map.setView([lat,lon]);
+L.easyButton('<img src="./crosshairs-gps.png">', function (btn, map) {
+    map.setView([lat, lon]);
 }).addTo(map);
 if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", function(event) {
+    window.addEventListener("deviceorientation", function (event) {
         // alpha: rotation around z-axis
         var rotateDegrees = event.alpha;
         // gamma: left to right
@@ -104,23 +104,23 @@ if (window.DeviceOrientationEvent) {
         handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
     }, true);
 }
-var handleOrientationEvent = function(frontToBack, leftToRight, rotateDegrees) {
-    if (frontToBack<30 && frontToBack>-30){
+var handleOrientationEvent = function (frontToBack, leftToRight, rotateDegrees) {
+    if (frontToBack < 30 && frontToBack > -30) {
         var weatherText = document.querySelector('a-text');
         weatherText.setAttribute('value', "vertical");
         var scene = document.querySelector('a-scene');
         scene.setAttribute('display', "none");
-        document.getElementById("map").style.display="flex";
+        document.getElementById("map").style.display = "flex";
 
     }
-    else{
+    else {
         var weatherText = document.querySelector('a-text');
         weatherText.setAttribute('value', "horizontal");
-        document.getElementById("map").style.display="none";
-        document.getElementById("scene").style.display="flex";
+        document.getElementById("map").style.display = "none";
+        document.getElementById("scene").style.display = "flex";
     }
 }
 
-function kelvinInCelsius(kelvin){
-    return kelvin-273.15;
+function kelvinInCelsius(kelvin) {
+    return kelvin - 273.15;
 }
---
+++ js/venues.js
@@ -0,0 +1,44 @@
+var position, scene = null;
+
+$(document).ready(() => {
+    navigator.geolocation.getCurrentPosition((x) => {
+        position = x;
+        scene = $('a-scene')[0];
+        getVenues();
+    });
+});
+
+function getVenues() {
+    const foursquare_url = 'https://api.foursquare.com/v2/venues/search?' +
+        'client_id=' + FOURSQUARE_ID +
+        '&client_secret=' + FOURSQUARE_SECRET +
+        '&v=20200528' +
+        '&ll=' + position.coords.latitude + ',' + position.coords.longitude;
+
+    $.ajax({
+        dataType: "json",
+        url: foursquare_url,
+        data: {},
+        success: function (data) {
+            var venues = data.response.venues;
+            venuesToAR(venues);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+        }
+    });
+}
+
+function venuesToAR(venues) {
+    venues.forEach((venue) => {
+        var latitude = venue.location.lat;
+        var longitude = venue.location.lng;
+        var icon = document.createElement('a-image');
+
+        icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+        icon.setAttribute('src', 'img/star-icon.png');
+        icon.setAttribute('look-at', '[gps-camera]');
+
+        scene.appendChild(icon);
+    });
+}
