1f83777370a9dbded5b3777985d89abb9423fcce
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-03 13:17:01 +0200

    Update buslines.js

--- js/buslines.js
+++ js/buslines.js
@@ -73,5 +73,5 @@ function createLine(arr) {
 function drawToAR(lines) {
     var line = document.createElement('a-entity');
     line.outerHTML = '<a-entity' + lines + '></<a-entity>';
-    scene.appendChild(line);
+    console.log(line.attributes());
 }
