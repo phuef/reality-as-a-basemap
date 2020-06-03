8302e15a46a9ba955be54585aeb0845f6b3f4fa2
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-03 12:50:42 +0200

    Update buslines.js

--- js/buslines.js
+++ js/buslines.js
@@ -61,14 +61,13 @@ function LineStringToAR(linestring) {
 };
 
 function createLine(arr) {
-    console.log(arr);
     var result = '';
     for (i = 0; i < arr.length; i++) {
         var line = 'line__' + [i] + '="start:' + arr[i] + '; end:' + arr[i + 1] + '; color: red" ';
         result.concat(line);
     }
 
-    //console.log(result);
+    console.log(result);
     //drawToAR(finalArr);
 }
 
