4baab68819e38ade80afb374bd99ab6db6662cab
phuef <phil.hueffer@gmail.com>
2020-06-05 14:16:14 +0200

    adjusting mapview

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ var mapview=false;
 //initialize leaflet map
 var map = L.map('map')
 mapLink = '<a href="http://www.esri.com/">Esri</a>';
-wholink = 'i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
 L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: '&copy; '+mapLink+', '+wholink,
     maxZoom: 20,
