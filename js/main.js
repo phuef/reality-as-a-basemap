fa6cd9e79e033932cc84e57b3e441fe69468c4bd
phuef <phil.hueffer@gmail.com>
2020-06-05 14:35:06 +0200

    adjusting mapview

--- js/main.js
+++ js/main.js
@@ -2,13 +2,27 @@ var current_position;
 var lat,lon;
 var mapview=false;
 
-//initialize leaflet map
-var map = L.map('map')
-mapLink = '<a href="http://www.esri.com/">Esri</a>';
-L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+var mapLink = '<a href="http://www.esri.com/">Esri</a>';
+
+var satelliteMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: '&copy; '+mapLink,
     maxZoom: 20,
-}).addTo(map);
+});
+var topoMap= L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+});
+//initialize leaflet map
+var map = L.map('map', {
+  layers: [satelliteMap]
+})
+var baseMaps = {
+  "Satellite": satelliteMap,
+  "Topographic": topoMap
+};
+L.control.layers(baseMaps).addTo(map);
+
+
+
 
 function onLocationFound(e) {
     if (current_position) {
