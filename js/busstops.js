08b4a7384a7f6e9c398e0510cc6469093c40cc9f
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-07 23:55:31 +0200

    Update busstops.js

--- js/busstops.js
+++ js/busstops.js
@@ -91,8 +91,8 @@ function busStopToPopup(busStop) {
     var distance = busStop.properties.distance;
 
     var html = '<i class="fas fa-bus fa-3x"></i><br><br><h5>' + name
-        + '</h5><br><i class="fas fa-map-signs fa-2x"></i>      stadt' + direction
-        + '<br><i class="fas fa-walking fa-2x"></i>     ' + distance + ' m';
+        + '</h5><br><i class="fas fa-map-signs fa-2x"></i>stadt' + direction
+        + '<br><i class="fas fa-walking fa-2x"></i>' + distance + ' m';
 
     return html;
 }
