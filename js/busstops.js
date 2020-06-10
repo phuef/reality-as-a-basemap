ed030ed345053234e061db4df9a1e1ceada5f083
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-10 18:18:24 +0200

    infobox

--- js/busstops.js
+++ js/busstops.js
@@ -70,11 +70,6 @@ function busStopsToAR(busStops) {
     });
 }
 
-function busStopToInfobox(event) {
-    var infobox = $("#infobox")[0];
-    infobox.innerHTML = event.target.getAttribute('name');
-}
-
 /** 
  * This function visualizes the bus stops as markers on the 2D map
  * @param {Array} busStops 
--- js/eventhandler.js
+++ js/eventhandler.js
@@ -1,5 +1,8 @@
 AFRAME.registerComponent('cursor_busstop', {
     init: function () {
-        this.el.addEventListener('mouseenter', busStopToInfobox(e));
+        this.el.addEventListener('mouseenter', (e) => {
+            var infobox = $("#infobox")[0];
+            infobox.innerHTML = e.target.getAttribute('name');
+        });
     }
 });
