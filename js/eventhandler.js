57f076923cb858933aaec2f07ae6a3ec1aaae332
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-10 15:17:19 +0200

    infobox

--- js/eventhandler.js
+++ js/eventhandler.js
@@ -1,6 +1,10 @@
 AFRAME.registerComponent('cursor_busstop', {
     init: function () {
         this.el.addEventListener('mouseenter', function () {
+            var infobox = $("#infobox");
+            var text = document.createElement("text");
+            text.attr("value", "TEST Bushaltestelle");
+            infobox.appendChild(text);
         });
     }
 });
