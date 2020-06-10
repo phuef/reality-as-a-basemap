b27628c83f575685c49783dc37ab52bf8574c03a
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-10 15:56:57 +0200

    infobox

--- js/eventhandler.js
+++ js/eventhandler.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent('cursor_busstop', {
     init: function () {
         this.el.addEventListener('mouseenter', function (e) {
             var infobox = $("#infobox")[0];
-            infobox.innerHTML = '<p>' + e.target.getAttribute('name') + '</p>';
+            infobox.innerHTML = e.target.getAttribute('name');
         });
     }
 });
