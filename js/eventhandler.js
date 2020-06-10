bd5af2d4ee30c41203a402789cf4bc8572f7bd02
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-10 15:30:36 +0200

    Update eventhandler.js

--- js/eventhandler.js
+++ js/eventhandler.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent('cursor_busstop', {
     init: function () {
         this.el.addEventListener('mouseenter', function () {
             var infobox = $("#infobox");
-            alert(infobox);
+            alert(infobox.innerHTML);
             infobox.innerHTML = "<p>TESTZEILE</p>";
         });
     }
