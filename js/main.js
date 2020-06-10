5dfab0da433f181ec392f02fe0c68b403d66a443
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-10 14:18:47 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ var mapview = false;
 var mapLink = '<a href="http://www.esri.com/">Esri</a>';
 
 var satelliteMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: '&copy; '+mapLink,
-    maxZoom: 20,
+  attribution: '&copy; ' + mapLink,
+  maxZoom: 20,
 });
-var topoMap= L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+var topoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 });
 //initialize leaflet map
 var map = L.map('map', {
@@ -19,35 +19,35 @@ var baseMaps = {
   "Satellite": satelliteMap,
   "Topographic": topoMap
 };
-var layerControl = L.control.layers(null, baseMaps, {position:'topright'}).addTo(map);
+var layerControl = L.control.layers(null, baseMaps, { position: 'topright' }).addTo(map);
 //L.control.layers(baseMaps).addTo(map);
 
 
 
 
 function onLocationFound(e) {
-    if (current_position) {
-        map.removeLayer(current_position);
-    }
-    current_position= L.marker(e.latlng).addTo(map);
+  if (current_position) {
+    map.removeLayer(current_position);
+  }
+  current_position = L.marker(e.latlng).addTo(map);
 
-    var latLngs = [ current_position.getLatLng() ];
-    lat=latLngs[0].lat;
-    lon=latLngs[0].lng;
-    //map.panTo(new L.LatLng(lat, lon));
-    init()
+  var latLngs = [current_position.getLatLng()];
+  lat = latLngs[0].lat;
+  lon = latLngs[0].lng;
+  //map.panTo(new L.LatLng(lat, lon));
+  init()
 
 }
 
 function onLocationError(e) {
-    alert(e.message);
+  alert(e.message);
 }
 map.on('locationfound', onLocationFound);
 map.on('locationerror', onLocationError);
 
 //relocates to the current position on the map
-function locate(){
-  if(mapview){
+function locate() {
+  if (mapview) {
     map.locate();
   }
 }
@@ -55,51 +55,50 @@ function locate(){
 
 // call locate every 5 seconds... forever
 //setInterval(init, 5000);
-function init(){
-    locate();
-    // hier werden die anderen Methoden aufgerufen
-    initVenues(lat, lon);
-    initBusstops(lat, lon);
-    initBuslines(lat, lon);
+function init() {
+  locate();
+  // hier werden die anderen Methoden aufgerufen
+  /* initVenues(lat, lon); */
+  initBusstops(lat, lon);
+  initBuslines(lat, lon);
 }
 
 if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", function(event) {
-        // alpha: rotation around z-axis
-        var rotateDegrees = event.alpha;
-        // gamma: left to right
-        var leftToRight = event.gamma;
-        // beta: front back motion
-        var frontToBack = event.beta;
+  window.addEventListener("deviceorientation", function (event) {
+    // alpha: rotation around z-axis
+    var rotateDegrees = event.alpha;
+    // gamma: left to right
+    var leftToRight = event.gamma;
+    // beta: front back motion
+    var frontToBack = event.beta;
 
-        handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-    }, true);
+    handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+  }, true);
 }
 // changes  the shown elements when the devices orientation changes
-var handleOrientationEvent = function(frontToBack, leftToRight, rotateDegrees) {
-  var checked= document.getElementById("checkboxSwitchView").checked;
-  if(checked){}
-    else{
-    if (frontToBack<30 && frontToBack>-30){
-        var scene = document.querySelector('a-scene');
-        scene.setAttribute('display', "none");
-        document.getElementById("dummerSlider").style.display="none";
-        document.getElementById("mapview").style.display="flex";
-        if (!mapview)
-        {
-          map.locate({setView: true, maxZoom: 20});
-        }
-        mapview=true;
+var handleOrientationEvent = function (frontToBack, leftToRight, rotateDegrees) {
+  var checked = document.getElementById("checkboxSwitchView").checked;
+  if (checked) { }
+  else {
+    if (frontToBack < 30 && frontToBack > -30) {
+      var scene = document.querySelector('a-scene');
+      scene.setAttribute('display', "none");
+      document.getElementById("dummerSlider").style.display = "none";
+      document.getElementById("mapview").style.display = "flex";
+      if (!mapview) {
+        map.locate({ setView: true, maxZoom: 20 });
+      }
+      mapview = true;
     }
-    else{
-        document.getElementById("mapview").style.display="none";
-        document.getElementById("scene").style.display="flex";
-        document.getElementById("dummerSlider").style.display="flex";
-        mapview = false;
+    else {
+      document.getElementById("mapview").style.display = "none";
+      document.getElementById("scene").style.display = "flex";
+      document.getElementById("dummerSlider").style.display = "flex";
+      mapview = false;
     }
   }
 }
 
-function kelvinInCelsius(kelvin){
-    return kelvin-273.15;
+function kelvinInCelsius(kelvin) {
+  return kelvin - 273.15;
 }
