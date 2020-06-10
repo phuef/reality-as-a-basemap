3aa578cf98c15d71d852e4e185e97ed8bf9241d0
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-10 17:45:23 +0200

    infobox

--- js/busstops.js
+++ js/busstops.js
@@ -70,6 +70,15 @@ function busStopsToAR(busStops) {
     });
 }
 
+/**
+ * 
+ * @param {*} event 
+ */
+function busStopToInfobox(event) {
+    var infobox = $("#infobox")[0];
+    infobox.innerHTML = e.target.getAttribute('name');
+}
+
 /** 
  * This function visualizes the bus stops as markers on the 2D map
  * @param {Array} busStops 
