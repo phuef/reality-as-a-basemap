2854df42833e9778130310a4cbb64e71bab050e3
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-10 14:11:45 +0200

    event handler

--- js/busstops.js
+++ js/busstops.js
@@ -1,23 +1,3 @@
-AFRAME.registerComponent('change-color-on-hover', {
-    schema: {
-        color: { default: 'red' }
-    },
-
-    init: function () {
-        var data = this.data;
-        var el = this.el;  // <a-box>
-        var defaultColor = el.getAttribute('material').color;
-
-        el.addEventListener('mouseenter', function () {
-            el.setAttribute('color', data.color);
-        });
-
-        el.addEventListener('mouseleave', function () {
-            el.setAttribute('color', defaultColor);
-        });
-    }
-});
-
 //Make the current position and the A-Frame scene object globally available
 var latitude, longitude, scene = null;
 
