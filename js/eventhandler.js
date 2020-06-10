cd18cc6895107057ce4910d9176a5b76a0390c43
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-10 14:57:46 +0200

    popup test

--- js/eventhandler.js
+++ js/eventhandler.js
@@ -1,7 +1,14 @@
 AFRAME.registerComponent('cursor_busstop', {
     init: function () {
         this.el.addEventListener('mouseenter', function () {
-            alert("Hallo!");
+            var popup = document.createElement("div");
+            var scene = $("scene")[0];
+
+            popup.attr("background", "grey");
+            popup.attr("opacity", "0.7");
+            popup.innerHTML = "Hallo, TEST";
+
+            scene.appendChild(popup);
         });
     }
 });
