d3a045007fb7de11f269f5333c949e3b8954b549
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-16 15:39:48 +0200

    Update popup-generator.js

--- js/popup-generator.js
+++ js/popup-generator.js
@@ -5,13 +5,10 @@ function generateBusStopPopup(busStop) {
     var lat = busStop.lat;
     var lon = busStop.lon;
 
-    return (`<i class="fas fa-bus fa-3x"></i><br>'
-        + name + '<br><br>'
-        + '<i class="fas fa-map-signs fa-2x"></i> stadt'
-        + direction + '<br>'
-        + '<i class="fas fa-walking fa-2x"></i> '
-        + distance + '<br><br>'
-        + '<a class="btn btn-success" href="#" onclick="navigate(' + lat + ', ' + lon + ')"><i class="fas fa-crosshairs"></i> Navigate</a>'`);
+    return (`<i class="fas fa-bus fa-3x"></i><br>${name}<br><br>`
+        + `<i class="fas fa-map-signs fa-2x"></i>${direction}<br>`
+        + `<i class="fas fa-walking fa-2x"></i>${distance}<br><br>`
+        + `<a class="btn btn-success" href="#" onclick="navigate(${lat},${lon})"><i class="fas fa-crosshairs"></i> Navigate</a>`);
 }
 
 function generateVenuePopup(venue) {
