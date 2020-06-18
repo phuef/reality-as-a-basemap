3f1cddbe5966f2ba380063db2b8d15d1672f8c17
phuef <phil.hueffer@gmail.com>
2020-06-18 14:55:42 +0200

    improve collapsible menu

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ var oldRadius=null;
 var mapLink = '<a href="http://www.esri.com/">Esri</a>';
 
 var satelliteMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: '&copy; '+mapLink
+    attribution: '&copy; '+mapLink,
+    maxZoom:20
 });
 var topoMap= L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
