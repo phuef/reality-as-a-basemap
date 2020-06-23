c26a3db491b37b5a74e0859589658ff59de05652
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-23 10:55:36 +0200

    Update busstops.js

--- js/busstops.js
+++ js/busstops.js
@@ -113,7 +113,7 @@ function filterBusStops(busStops) {
         var lat2 = busStop.geometry.coordinates[1];
         var lon2 = busStop.geometry.coordinates[0];
 
-        console.log(lat2, lon2);
+        console.log(lat1, lon1);
 
         var distance = getDistance(lat1, lon1, lat2, lon2); //Calculate the distance between the user's position and the bus stop
         busStop.properties.distance = distance; //Store the distance within the GeoJSON object
